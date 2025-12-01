49ac6e0ef120a2eb0f1453865d4bf38ed0d3be3c
reihanesheikheezade <reihane.sheikheezade@gmail.com>
2025-12-01 05:31:35 -0800

    searchProducts and sortProducts methodes added to ProductView

--- src/js/ProductView.js
+++ src/js/ProductView.js
@@ -4,10 +4,14 @@ const addNewProductBtn = document.querySelector("#add-new-product");
 const productTitle = document.querySelector("#product-title");
 const productQuantity = document.querySelector("#product-quantity");
 const productCategory = document.querySelector("#product-category");
+const searchInput = document.querySelector("#search-input");
+const sortInput = document.querySelector("#sort-input");
 
 class ProductView {
   constructor() {
     addNewProductBtn.addEventListener("click", (e) => this.addNewProduct(e));
+    searchInput.addEventListener("input", (e) => this.searchProducts(e));
+    sortInput.addEventListener("change", (e) => this.sortProducts(e));
     this.products = [];
   }
 
@@ -59,6 +63,28 @@ class ProductView {
   }
   getProducts() {
     this.products = Storage.getAllProducts();
+  }
+  searchProducts(e) {
+    const value = e.target.value.trim().toLowerCase();
+
+    const filteredProducts = Storage.getAllProducts().filter((p) =>
+      p.title.toLowerCase().includes(value)
+    );
+    this.createProductsList(filteredProducts);
+  }
+  sortProducts(e) {
+    const selectedSort = e.target.value;
+    let sortedProducts = [];
+    if (selectedSort === "newest") {
+      sortedProducts = this.products.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      );
+    } else {
+      sortedProducts = this.products.sort(
+        (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+      );
+    }
+    this.createProductsList(sortedProducts);
   }
 }
 
